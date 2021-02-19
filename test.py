3ba41b308754c3f7b542a61ce62de1bb5e42531b
Zhang, Chen <zhangc@ornl.gov>
2021-02-18 19:50:37 -0500

    updated testing script

--- test.py
+++ test.py
@@ -1,14 +1,31 @@
 from mantid.simpleapi import *
 import numpy as np
 
-import os 
+import os
 
-ws = LoadIsawPeaks('data/Natrolite_runs_133752_133812.peaks')
+pws = LoadIsawPeaks('data/Natrolite_runs_133752_133812.peaks', )
 
 FilterPeaks(
-    InputWorkspace=ws,
-    OutputWorkspace=ws,
-    FIlterVariable='BankName',
-    FilterValue=17,
-    Operator'=',
-    )
+    InputWorkspace="pws",
+    OutputWorkspace="pws_intensity",
+    FIlterVariable='Intensity',
+    FilterValue=100,
+    Operator='>',
+)
+
+FilterPeaks(
+    InputWorkspace="pws",
+    OutputWorkspace="pws_bank7",
+    BankName='bank7',
+    Criterion='=',
+)
+
+FilterPeaks(
+    InputWorkspace="pws",
+    OutputWorkspace="pws_intensity_bank7",
+    FIlterVariable='Intensity',
+    FilterValue=10,
+    Operator='>',
+    BankName='bank7',
+    Criterion='=',
+)
